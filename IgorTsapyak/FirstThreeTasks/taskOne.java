acaa35b991934dd06b6ba0ddc2d5acb4c0716d8e
New Commit <xpertcv@gmail.com>
2016-01-18 10:37:25 +0200

    AddedFirstTsakWithDoWhileUse

---
+++ IgorTsapyak/FirstThreeTasks/taskOne.java
@@ -0,0 +1,21 @@
+/**
+ * Знайти добуток n значного числа.
+ */
+
+public class taskOne {
+
+    public static void main(String[] args) {
+
+        int counter = 10;
+        int var = 2369;
+        int resultVar;
+        int outVar = 1;
+        do {
+        resultVar = var%counter;
+        var=var/counter;
+        outVar*=resultVar;
+            }while (var%counter>0);
+        System.out.println(outVar);
+        }
+    }
+
