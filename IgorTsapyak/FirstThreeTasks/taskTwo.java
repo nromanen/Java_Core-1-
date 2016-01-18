86b4738eab2a3737b4e25289f0ab60d7a95e73b5
New Commit <xpertcv@gmail.com>
2016-01-18 11:10:40 +0200

    CommitWithTsakTwoAdd

---
+++ IgorTsapyak/FirstThreeTasks/taskTwo.java
@@ -0,0 +1,27 @@
+
+public class taskTwo {
+
+    public static void main(String[] args) {
+
+        int counter = 10;
+        int var = 1023669874;
+        int resultVal;
+        int outVar1 = 0;
+        int outVar2 = 0;
+        do {
+            resultVal = var%counter;
+            var=var/counter;
+            if (resultVal % 2 == 0 ){
+                outVar1 += resultVal;
+            }else {
+                outVar2 +=resultVal;
+            }
+
+        }while (var!=0);
+
+        System.out.println("the Amount of even numbers = " + outVar1);
+        System.out.println("The Amount of odd numbers = " + outVar2);
+
+    }
+
+}
