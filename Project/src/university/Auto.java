141c58fc5b0df24f02c705620558469f682331af
nromanen <nromanen.softserveinc@gmail.com>
2016-02-03 20:29:32 +0200

    one More 03.02

--- Project/src/university/Auto.java
+++ Project/src/university/Auto.java
@@ -7,9 +7,12 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Auto {
 
 	private String model;
+	
 	private int year;
 	private double price;
 	private String colour;
@@ -61,118 +64,47 @@ public class Auto {
 		return "[" + model + "," + year + " year (" + price + "), colour="
 				+ colour + "]";
 	}
+	
+	
+	/**
+	 * 
+	 * @param autoRep 
+	 * @return
+	 * @throws AutoFormatException
+	 */
+	public static Auto valueOf(String autoRep) throws AutoFormatException{
+		Auto auto = new Auto();
+		String[] err = AutoValidator.errors(autoRep);
+		String[] strings = StringUtils.split(autoRep, ", .;[]()");
+
+		if (!(AutoValidator.errors(autoRep)[0]).isEmpty()) {
+			throw new AutoFormatException(err);
+		} else
+			auto.setModel(strings[0]);
+
+/*		if (!(AutoValidator.errors(autoRepresentation)[1]).isEmpty()) {
+			return null;
+		} else {
+			auto.setYear(Integer.parseInt(strings[1]));
+		}
+*/
+		auto.setYear(8765);
+		auto.setColour("Blue");
+		auto.setPrice(34.5);
+		return auto;
+
+			}
 
 	public static void main(String... strings) {
-
-		List<Auto> list = new ArrayList<>();
-
-		Auto a1 = new Auto();
-		a1.setColour("fdd");
-		a1.setModel("Model1");
-		a1.setPrice(34567);
-		a1.setYear(2016);
-
-		Auto a2 = new Auto();
-		a2.setColour("fdd");
-		a2.setModel("Model2");
-		a2.setPrice(100);
-		a2.setYear(2012);
-
-		list.add(a1);
-		list.add(a2);
-
-		// System.out.println(a1 + " =???" + a2 + a1.equals(a2));
-
-		System.out.println(list);
-		// sort by year
-		/*
-		 * compare auto by year
-		 */
-		/*
-		 * Collections.sort(list, new Comparator<Auto>(){
-		 * 
-		 * @Override public int compare(Auto arg0, Auto arg1) { return
-		 * arg0.getYear() - arg1.getYear(); }
-		 * 
-		 * });
-		 */
-		Collections.sort(list, new CompareAuto("YEAR BY ASC"));
-		Collections.sort(list, new CompareAuto("PRICE BY ASC"));
 		
-		//System.out.println(list);
+		//System.out.println(Auto.valueOf("Rt , 65"));
 		
-		String template = "hdkjf.;kiuh   djfk";
-		String[] words = template.split("[\\s\\.;,]");
-		List<String> listWords = 
-				new ArrayList<>(Arrays.asList(words));
-		Iterator<String> iter = listWords.iterator();
-		while (iter.hasNext()){
-			if (iter.next().isEmpty()){
-				iter.remove();
-			}
+		Auto a = new Auto();
+		try {
+			a = Auto.valueOf("dfdf");
+		} catch (AutoFormatException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
 		}
-		System.out.println(listWords);
-
-		String template2 = "hd4k  kiu89h   djfk";
-		String[] words2 = template2.split("[\\s\\.;,]");
-		List<String> listWords2 = 
-				new ArrayList<>(Arrays.asList(words2));
-		iter = listWords2.iterator();
-		while (iter.hasNext()){
-			if (iter.next().isEmpty()){
-				iter.remove();
-			}
-		}
-		System.out.println(listWords2);
-
-		boolean flag = true;
-		for(int i = 0; i < listWords.size(); i++){
-			if (!(listWords.get(i).substring(0, 3).
-					equals(listWords2.get(i).substring(0, 2)))){
-				flag=false;
-				break;
-			}
-				
-		}
-		
-		if (flag) 
-			System.out.println("OK");
-
-		
-	}
-}
-
-class CompareAutoByYear implements Comparator<Auto> {
-
-	@Override
-	public int compare(Auto arg0, Auto arg1) {
-		return arg0.getYear() - arg1.getYear();
-	}
-}
-
-class CompareAuto implements Comparator<Auto> {
-
-	public String criteria;
-
-	public CompareAuto() {
-	}
-
-	public CompareAuto(String crit) {
-		criteria = crit;
-	}
-
-	@Override
-	public int compare(Auto arg0, Auto arg1) {
-		if ("YEAR".equals(criteria)) {
-
-			return arg0.getYear() - arg1.getYear();
-		} 
-		
-		else
-			if ("PRICE".equals(criteria)) {
-
-			return (int)(arg0.getPrice() - arg1.getPrice());
-	}
-return 0;
 }
 }
--- Project/src/university/AutoBuilder.java
+++ Project/src/university/AutoBuilder.java
@@ -2,79 +2,87 @@ package university;
 
 import static university.Utils.isValidString;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
+
 
 public class AutoBuilder {
 
-	/*public static Auto valueOf(String autoRepresentation){
+	public static Auto valueOf(String autoRepresentation) {
 		Auto auto = new Auto();
-		String[] string = autoRepresentation.split(",\\s");
-		auto.setModel(string[0]);
-		auto.setYear(Integer.parseInt(string[1]));
-		return auto;
-	}*/
-	
-	public static Auto valueOf(String autoRepresentation){
-		Auto auto = new Auto();
-		
-		String Input_Pattern_Full_Match = "(.*),\\s(.*)";
 
-		Pattern p2 = Pattern.compile(Input_Pattern_Full_Match);
-		Matcher m2 = p2.matcher(autoRepresentation);
-
-		m2.matches();
-
-				
-	//	String s = AutoValidator.errors(autoRepresentation)[0];
+/*		String[] array = Arrays.asList(autoRepresentation.split("[,]"))
+				.stream().filter(str -> !str.isEmpty())
+				.collect(Collectors.toList()).toArray(new String[0]);
+*/
 		
-	//	System.out.println(autoRepresentation + " ***" +s);
-			if (!(AutoValidator.errors(autoRepresentation)[0]).isEmpty()){
-				return null;
-			}
-			else 
-				auto.setModel(m2.group(1));
-		
-	if (!(AutoValidator.errors(autoRepresentation)[1]).isEmpty()){
-		return null;
-	}
-		else {
-			auto.setYear(Integer.parseInt(m2.group(2)));
+		String[] strings = StringUtils.split(autoRepresentation, ", .;[]()");
+
+		if (!(AutoValidator.errors(autoRepresentation)[0]).isEmpty()) {
+			return null;
+		} else
+			auto.setModel(strings[0]);
+
+		if (!(AutoValidator.errors(autoRepresentation)[1]).isEmpty()) {
+			return null;
+		} else {
+			auto.setYear(Integer.parseInt(strings[1]));
 		}
-	
-			auto.setColour("Blue");
+
+		auto.setColour("Blue");
 		auto.setPrice(34.5);
 		return auto;
 	}
-	
-	
-	public static void main(String ...strings ){
-		Auto a = AutoBuilder.valueOf("yMuyu, 987");
-		/*a.setColour("Red");
-		a.setPrice(456);*/
-		System.out.println(a);
-/*		
-		Pattern pattern = Pattern.compile("([a-z]+)([0-9])");
-		Matcher matcher = pattern.matcher("67yt23");
-		matcher.find();
 
-		System.out.println(matcher.group(1));
-		System.out.println(matcher.group(2));
-*/
+	public static Auto buildFromString(String autoRepresentation)
+			throws AutoFormatException {
+		for (String s : AutoValidator.errors(autoRepresentation)) {
+			if (!s.isEmpty()) {
+				throw new AutoFormatException(
+						AutoValidator.errors(autoRepresentation));
+			}
+		}
+		return AutoBuilder.valueOf(autoRepresentation);
+	}
+
+	public static void main(String... strings) {
+		try {
+			System.out.println(buildFromString("Tiui, 1223"));
+			//System.out.println(buildFromString("Tiui, 12"));
+			System.out.println(buildFromString("Tiui [1256"));
+		} catch (AutoFormatException autoFormat) {
+			System.out.println(autoFormat);
+		}
+
+		Auto a = AutoBuilder.valueOf("M3uyu, 987");
+		/*
+		 * a.setColour("Red"); a.setPrice(456);
+		 */
+		System.out.println(a);
+		/*
+		 * Pattern pattern = Pattern.compile("([a-z]+)([0-9])"); Matcher matcher
+		 * = pattern.matcher("67yt23"); matcher.find();
+		 * 
+		 * System.out.println(matcher.group(1));
+		 * System.out.println(matcher.group(2));
+		 */
 		Scanner scn = new Scanner(System.in);
 		a = AutoBuilder.valueOf(scn.nextLine());
 		String model = "RTY";
-		 if (Utils.isValidString(model, "MODEL_PATTERN")){
-			 a.setModel(model);
-		 }
-		
-		//String[] string = AutoValidator.errors("Mui, 987");
-		//System.out.println(Arrays.toString(string));
-		
+		if (Utils.isValidString(model, "MODEL_PATTERN")) {
+			a.setModel(model);
+		}
+
+		// String[] string = AutoValidator.errors("Mui, 987");
+		// System.out.println(Arrays.toString(string));
+
 	}
 }
 
-
-
-//Exception for invalidFormat 
+// Exception for invalidFormat 
