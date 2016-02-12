b0d27c7e43d10a36b134f47aa130e9b13cd98044
nromanen <nromanen.softserveinc@gmail.com>
2016-02-12 07:48:06 +0200

    Work with DB

---
+++ Project/src/zoo/Animal.java
@@ -0,0 +1,22 @@
+package zoo;
+
+public class Animal {
+	private int id;
+public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
